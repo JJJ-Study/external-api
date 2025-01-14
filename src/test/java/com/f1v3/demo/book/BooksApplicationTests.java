f175f645209f3916142e8ca1e0599c7016e5c3bd
seungjo <f1v3@kakao.com>
2025-01-14 17:15:25 +0900

    first commit

---
+++ src/test/java/com/f1v3/demo/book/BooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f1v3.demo.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
